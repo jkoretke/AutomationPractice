f8035fc531f14976bd0eb5426c3d937973d39ca5
Jeffrey Koretke <jeffreykoretke@Jeffreys-MacBook-Pro.local>
2018-09-04 16:04:33 -0700

    started ChuckNorrisFact.java

---
+++ app/src/main/java/com/example/jeffreykoretke/automationpractice/ChuckNorrisFact.java
@@ -0,0 +1,22 @@
+package com.example.jeffreykoretke.automationpractice;
+
+public class ChuckNorrisFact {
+
+    private String picURL;
+    private String joke;
+
+    public ChuckNorrisFact(){
+        this.picURL = "";
+        this.joke = "";
+
+    }
+
+    public ChuckNorrisFact(String pic, String joke){
+        this.picURL = pic;
+        this.joke = joke;
+    }
+    
+
+
+
+}
